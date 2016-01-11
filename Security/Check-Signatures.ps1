01e9ee9a613dd0a4cf9bcb9cd4724f29e256ec92
DanielRTeixeira <DanielRTeixeira@users.noreply.github.com>
2016-01-11 17:41:27 +0000

    Update and rename VerifySignatures.ps1 to Check-Signatures.ps1

---
+++ Security/Check-Signatures.ps1
@@ -0,0 +1,24 @@
+function Check-Signatures {
+
+    function CheckAdmin{
+        If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+        {
+            Write-Warning "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
+            Break
+        }
+    }
+    function Verify {
+    
+        $ErrorActionPreference = "SilentlyContinue"
+        $disk = gwmi win32_logicaldisk -Filter "DriveType='3'"
+        $drive = $disks.DeviceID +"\"
+    
+        foreach($e in $disk)
+        {
+            $file = gci $drive -Recurse -Include "*.exe"
+            Get-AuthenticodeSignature $file | where {$_.Status -eq "NotSigned"}
+        }
+    }
+    CheckAdmin
+    Verify
+}
