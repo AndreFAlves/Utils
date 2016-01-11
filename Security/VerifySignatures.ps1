861800ce68fea7f6abb16a8c515159bb6c8d3078
DanielRTeixeira <DanielRTeixeira@users.noreply.github.com>
2016-01-11 17:28:50 +0000

    Update VerifySignatures.ps1

--- Security/VerifySignatures.ps1
+++ Security/VerifySignatures.ps1
@@ -1,15 +1,20 @@
-If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
-{
-    Write-Warning "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
-    Break
-}
+function Verify-Signatures {
+    function Check-Admin {
+        If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+        {
+            Write-Warning "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
+            Break
+        }
+    }
 
-$ErrorActionPreference = "SilentlyContinue"
-$disk = gwmi win32_logicaldisk -Filter "DriveType='3'"
-$drive = $disks.DeviceID +"\"
-foreach($e in $disk)
-
-{
-    $file = gci $drive -Recurse -Include "*.exe"
-    Get-AuthenticodeSignature $file | where {$_.Status -eq "NotSigned"}
+function Verity {
+    $ErrorActionPreference = "SilentlyContinue"
+    $disk = gwmi win32_logicaldisk -Filter "DriveType='3'"
+    $drive = $disks.DeviceID +"\"
+    
+    foreach($e in $disk)
+    {
+        $file = gci $drive -Recurse -Include "*.exe"
+        Get-AuthenticodeSignature $file | where {$_.Status -eq "NotSigned"}
+    }
 }
